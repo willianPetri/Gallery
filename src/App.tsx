37bc60621673a3e9d5d92047b6c1438383a6043e
willian <wopetri11@gmail.com>
2021-12-11 13:33:06 -0400

    Added PhotoItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles'
+import * as Photos from './services/photos';
+import { Photo } from './types/photo';
+import { PhotoItem } from './components/PhotoItem';
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const getPhotos = async() => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, [])
+
   return (
     <C.Container>
       <C.Area>
         <C.Header>Galeria de Fotos</C.Header>
 
-        {/* Upload de fotos */}
+        {loading &&
+          <C.ScreenWarning>
+            <div className='emoji'>🤚</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
 
-        {/* Lista de fotos */}
+        {!loading && photos.length > 0 && 
+          <C.PhotoList>
+            {photos.map((item, index) => (
+              <PhotoItem key={index} url={item.url} name={item.name} />
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 &&
+          <C.ScreenWarning>
+            <div className='emoji'>😞</div>
+            <div>Não há fotos cadastradas.</div>
+        </C.ScreenWarning>
+        }
       </C.Area>
     </C.Container>
   );
